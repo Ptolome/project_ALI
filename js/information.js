02540ea32a56be61aa6a70009b8923bbbf84f342
Ptolome <74111131+Ptolome@users.noreply.github.com>
2023-08-05 09:36:51 +0300

    Add files via upload

---
+++ js/information.js
@@ -0,0 +1,8 @@
+const dicter ={
+    0:['Наш офис','Our oficces','Our oficces' ],
+    1:['Услуги','service','service' ],
+    2:['Вакнсиис','vacansii','vacansii' ],
+    3:['Контакты','contacts','contacts' ]
+  }
+
+  export default dicter
